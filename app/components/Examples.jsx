49337c2b603053562bc83f8306e8d46860f87652
Aditya Kalluraya <adityakalluraya@gmail.com>
2017-02-17 20:41:13 +0530

    Added a copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 var Examples = React.createClass({
   render: function() {
     return(
+      <div>
       <h3>Examples comp</h3>
+      <p>Welcome to examples page</p>
+      </div>
     )
   }
 });
