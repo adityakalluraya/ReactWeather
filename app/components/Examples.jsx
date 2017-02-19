454b87ee282896a20f8fbad7a653a5a578353211
Aditya Kalluraya <adityakalluraya@gmail.com>
2017-02-19 13:25:08 +0530

    Update Example page and some styles

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-    <h3>Examples</h3>
-    <p>This is an example page</p>
+    <h1 className ="text-center">Examples</h1>
+    <p>Here are a few example locations to try out:</p>
+    <ol>
+      <li>
+        <Link to='/?location=Puttur'>Puttur</Link>
+      </li>
+      <li>
+        <Link to='/?location=Bangalore'>Bangalore</Link>
+      </li>
+    </ol>
     </div>
   )
 };
