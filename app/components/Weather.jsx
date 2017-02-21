8b5440aaa2e981db6ff3cba2b8f194c23f0a3747
Aditya Kalluraya <adityakalluraya@gmail.com>
2017-02-21 11:35:59 +0530

    Minor error corrections for prev commit

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -32,23 +32,23 @@ var Weather = React.createClass({
         errorMessage: e.message
       });
     });
-    },
-    componentDidMount: function () {
-      var location = this.props.location.query.location;
+  },
+  componentDidMount: function () {
+    var location = this.props.location.query.location;
 
-      if(location && location.length>0) {
-        this.handleSearch(location);
-        window.location.hash ='#/';
-      }
-    },
-    componentWillReceiveProps: function(newProps) {
-      var location = newProps.location.query.location;
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function (newProps) {
+    var location = newProps.location.query.location;
 
-      if(location && location.length>0) {
-        this.handleSearch(location);
-        window.location.hash ='#/';
-      }
-    },
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
   render: function () {
     var {isLoading, temp, location, errorMessage} = this.state;
 
